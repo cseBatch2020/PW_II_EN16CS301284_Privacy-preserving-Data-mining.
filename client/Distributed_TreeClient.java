f10f0eb30f00876c5b2d3dbb04d6a3b0eddeec12
Vasudha Yadav <vasudhayadav560@gmail.com>
2020-06-07 11:30:02 +0530

    Add files via upload

---
+++ client/Distributed_TreeClient.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package distributed_treeclient;
+
+/**
+ *
+ * @author maxtech
+ */
+public class Distributed_TreeClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       new MainFrame().setVisible(true);
+    }
+    
+}
